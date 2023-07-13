f5dff7a32dc4a8367e2bc00aff83d1b58d2d786e
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2023-07-13 23:42:33 +0530

    Create q.asm

---
+++ LAB VII: Analyze and evaluate different Array operations using ARM processor./Separate Even numbers and odds numbers in array /q.asm
@@ -0,0 +1,31 @@
+// Q3. Separate Even numbers and odds numbers in an array of size N.
+
+
+.global _start
+_start:
+
+ldr r0, =count
+ldr r1, [r0]
+ldr r3, =array
+ldr r4, =even
+ldr r5, =odd
+
+back: ldr r6, [r3], #4
+ands r7, r6, #1
+beq fwd
+str r6, [r5], #4
+b fwd1
+
+fwd: str r6, [r4], #4
+
+fwd1: subs r1, r1, #01
+bne back
+
+exit:b exit
+
+.data
+count:.word 0x07
+array:.word 0x15, 0x35, 0x32, 0x45, 0x10, 0x4f, 0x34
+even:.word 0, 0, 0, 0, 0
+odd:.word 0, 0, 0, 0, 0
+Result:
