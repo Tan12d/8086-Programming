42428ef050a798d6c261e2d111a874b0d1fc7623
Tanmoy Das <100254217+Tan12d@users.noreply.github.com>
2023-07-13 23:34:05 +0530

    Update q.asm

--- LAB VII: Analyze and evaluate different Array operations using ARM processor./Smallest in array/q.asm
+++ LAB VII: Analyze and evaluate different Array operations using ARM processor./Smallest in array/q.asm
@@ -1,1 +1,26 @@
+// Q1. Find the smallest number in a given array of size N.
 
+
+.global _start
+_start:
+
+ldr r0, =count
+ldr r1, [r0]
+mov r4, #0x00
+ldr r2, =array
+
+back: ldr r3, [r2], #4
+cmp r4, r3
+blt fwd
+mov r4, r3
+
+fwd: subs r1, r1, #01
+bne back 
+str r4, [r2]
+
+exit: b exit
+
+.data
+count:.word 0x05
+array:.word 0x15,0x35,0x45,0x10,0x4f
+Result:
